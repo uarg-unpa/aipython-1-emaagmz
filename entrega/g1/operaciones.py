91112923e89469b8f2d1411c2799ade47f417ed0
emaagmz <91705362+emaagmz@users.noreply.github.com>
2023-10-23 19:03:07 +0000

    actualizo

---
+++ entrega/g1/operaciones.py
@@ -0,0 +1,9 @@
+print(type(23))
+
+print(22+23)
+print(22-33)
+print(22/33)
+print(22*33)
+print(22%33)
+print(22//33)
+print(22**33)
--- entrega/g1/viernes.py
+++ entrega/g1/viernes.py
@@ -23,3 +23,21 @@ print("Mensaje propio")
 print("23", 23)
 print(" \"Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano.\" ")
 print("Emanuel", "Gomez", 22)
+print("Calle\t", "1919\t", "Código Postal 9400\t")
+print("Calle\n", "1919\n", "Código Postal 9400\n")
+
+texto = "Feliz \nPrimavera 2\t 0\t 2\t 3\t"
+print(texto)
+
+msj1 = "Solo podemos ver poco del futuro,"
+msj2 = "pero lo suficiente para darnos cuenta de que hay mucho que hacer."
+print(msj1, msj2)
+
+print(" *")
+print(" * *")
+print(" * *")
+print(" * *")
+print("*** ***")
+print(" * *")
+print(" * *")
+print(" *****")
