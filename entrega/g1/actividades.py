3449d6d6c437627ced28d5fe2c964aadc4f17f66
emaagmz <91705362+emaagmz@users.noreply.github.com>
2023-10-23 19:43:06 +0000

    actualizacion

---
+++ entrega/g1/actividades.py
@@ -0,0 +1,10 @@
+nombre = "Emanuel"
+apellido = "Gomez"
+edad = 22
+altura = "1.80"
+num_vuelo = 2777
+temp = "22°"
+salario_mensual = "2200"
+game_over = ""
+num_par = 2
+
--- entrega/g1/operaciones.py
+++ entrega/g1/operaciones.py
@@ -1,6 +1,6 @@
 print(type(23))
 
-print(22+23)
+print(22+33)
 print(22-33)
 print(22/33)
 print(22*33)
