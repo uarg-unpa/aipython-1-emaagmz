e5d293f0f995b54db7c8e2667337e3daa3bfdfb9
emaagmz <91705362+emaagmz@users.noreply.github.com>
2023-10-23 18:19:41 +0000

    actualizo

---
+++ entrega/g1/viernes.py
@@ -0,0 +1,25 @@
+print("Viernes en AIPython")
+
+# Comentario 
+"""
+print("Taller", "AIPython", "viernes")
+
+print("", sep=" ", end="\n")
+
+print("Esto es un \n salto de \n línea")
+
+print("Esto es \t un comentario \t tabulado")
+
+print("La funcion \"print\" ...")
+
+print('La funcion "print" ...') 
+"""
+
+
+
+print("Las máquinas me sorprenden con mucha frecuencia.")
+print("")
+print("Mensaje propio")
+print("23", 23)
+print(" \"Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano.\" ")
+print("Emanuel", "Gomez", 22)
