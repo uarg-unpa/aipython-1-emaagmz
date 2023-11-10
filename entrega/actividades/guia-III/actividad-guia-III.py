97e7698fd7fff6be641dba3a905cc09fe55296b3
emaagmz <91705362+emaagmz@users.noreply.github.com>
2023-11-10 20:26:43 +0000

    actualizar

---
+++ entrega/actividades/guia-III/actividad-guia-III.py
@@ -0,0 +1,25 @@
+# ACTIVIDADES GUÍA III
+
+# 1
+i = 0
+while i <= 100:
+    print(i)
+    i += 1
+
+# 2
+for i in range(101):
+    print(i)
+
+# 3
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+
+# 4
+numero1 = int(input("Ingrese el primer número entero: "))
+numero2 = int(input("Ingrese el segundo número entero: "))
+for i in range(numero1, numero2 + 1):
+    print(i)
+
+# end=' '
